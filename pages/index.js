a4a4a4edf8a1a15c3cf04405167d7048b2f97bee
AdityA <84802479+2802AdityA@users.noreply.github.com>
2022-10-21 04:04:54 +0530

    Final

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { Metaplex } from '@metaplex-foundation/js';
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
 
-const connection = new Connection(clusterApiUrl('mainnet-beta'));
+const connection = new Connection(clusterApiUrl('devnet'));
 const mx = Metaplex.make(connection);
 
-const Home = () => {
+export default function Home() {
   const [address, setAddress] = useState(
-    'Geh5Ss5knQGym81toYGXDbH3MFU2JCMK7E4QyeBHor1b',
+    'EAqjUWVX2m9fdfGNBzTY5zSiid1Sb9V3x6EL8ssZBTkw',
   );
 
   const [nftList, setNftList] = useState(null);
@@ -19,7 +19,15 @@ const Home = () => {
   const perPage = 1;
 
   const fetchNFTs = async () => {
-    // add some code here
+    try {
+      setLoading(true);
+      setCurrentView(null);
+      const list = await mx.nfts().findAllByOwner({ owner: new PublicKey(address)});
+      setNftList(list);
+      setCurrentPage(1);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   useEffect(() => {
@@ -27,15 +35,32 @@ const Home = () => {
       return;
     }
 
-    // add some code here
+    const execute = async () => {
+      const startIndex = (currentPage - 1) * perPage;
+      const endIndex = currentPage * perPage;
+      const nfts = await loadData(startIndex, endIndex);
+
+      setCurrentView(nfts);
+      setLoading(false);
+    };
+
+    execute();
   }, [nftList, currentPage]);
 
   const loadData = async (startIndex, endIndex) => {
-    // add some code here
+    const nftsToLoad = nftList.filter((_, index) => (index >= startIndex && index < endIndex))
+
+    const promises = nftsToLoad.map((metadata) => mx.nfts().load({ metadata }));
+    return Promise.all(promises);
   };
 
   const changeCurrentPage = (operation) => {
-    // add some code here
+    setLoading(true);
+    if (operation === 'next') {
+      setCurrentPage((prevValue) => prevValue + 1);
+    } else {
+      setCurrentPage((prevValue) => (prevValue > 1 ? prevValue - 1 : 1));
+    }
   };
 
   return (
@@ -67,7 +92,7 @@ const Home = () => {
                 <h1>{nft.name}</h1>
                 <img
                   className={styles.nftImage}
-                  src={nft.metadata.image || '/fallbackImage.jpg'}
+                  src={nft?.json?.image || '/fallbackImage.jpg'}
                   alt="The downloaded illustration of the provided NFT address."
                 />
               </div>
@@ -83,9 +108,7 @@ const Home = () => {
                 Prev Page
               </button>
               <button
-                disabled={
-                  nftList && Math.ceil(nftList.length / perPage) === currentPage
-                }
+                disabled={nftList && nftList.length / perPage === currentPage}
                 className={styles.styledButton}
                 onClick={() => changeCurrentPage('next')}
               >
@@ -97,6 +120,4 @@ const Home = () => {
       </div>
     </div>
   );
-};
-
-export default Home;
+}
